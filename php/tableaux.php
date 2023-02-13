39698b23542ee7f263bc9eb196d98b24e84f100f
Jordan Olanier <olanier.jordan@orange.fr>
2023-02-13 21:36:15 +0100

    mise en place du tableau

---
+++ php/tableaux.php
@@ -0,0 +1,13 @@
+$fiches = [
+    'Tamarin lion doré' => [
+        'image' => "https://upload.wikimedia.org/wikipedia/commons/f/fd/Tamarin-lion_%28Biodome%29.jpg"
+        'nom' => "Tamarin lion doré",
+        'scientifique' => "Leontopithecus rosalia",
+        'classe' => "Mammalia",
+        'ordre' => "Primates",
+        'famille' => "Callitrichidae",
+        'genre' => "Leontopithecus",
+        'taille' => "26 cm en moyenne (37cm de queue en moyenne)",
+        'territoire' => "Brésil",
+        'conservation' => "En danger"],
+    ]
